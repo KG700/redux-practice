0b30b194b0d539738ac7b9b0a14219c3167032cb
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-27 15:21:32 +0100

    Updated reducer to use switch case

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,15 +3,16 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'ADD_PERSON') {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        return {
-            persons: state.persons.concat(newPerson)
-        }
+    switch (action.type) {
+        case 'ADD_PERSON':
+            const newPerson = {
+                id: Math.random(), // not really unique but good enough here!
+                name: 'Max',
+                age: Math.floor( Math.random() * 40 )
+            }
+            return {
+                persons: state.persons.concat(newPerson)
+            }
     }
     return state;
 }
